95fab6de113d0607fef7035820b1e756ecbe0881
Enrique1311 <ejspinelli131176@gmail.com>
2023-04-11 19:50:24 -0300

    Se agregaron más temas: ejercicios básicos, módulos, asincronía y nuevos tipos de datos.

---
+++ js/constantes.js
@@ -0,0 +1,19 @@
+/* **********   Módulos ( import / export )   ********** */
+export const PI = Math.PI;
+
+export let usuario = "Jon";
+
+const password = "qwerty";
+//export default password;
+
+const hello = () => console.log("Hola");
+
+export default function saludar() {
+	console.log("Hola Módulos +ES6");
+}
+
+export class Saludar {
+	constructor() {
+		console.log("Hola Clases +ES6");
+	}
+}
