950958b5473da284c6901fb1e774ef4408a873d7
Enrique1311 <ejspinelli131176@gmail.com>
2023-04-17 20:46:02 -0300

    Reloj y reproductor de música

--- ejercicios-dom/js/index-dom.js
+++ ejercicios-dom/js/index-dom.js
@@ -1,3 +1,4 @@
+// Mobile Menu *************************************************
 const $menuBtn = document.querySelector(".menu-btn");
 const $mobileNavbar = document.querySelector(".mobile-navbar");
 const $openIcon = document.querySelector(".open-icon");
@@ -11,3 +12,49 @@ function openCloseMenu() {
 
 $menuBtn.addEventListener("click", openCloseMenu);
 $mobileNavbar.addEventListener("click", openCloseMenu);
+
+// Clock and Music **********************************************
+const $clockInit = document.querySelector(".clock-init");
+const $clockStop = document.querySelector(".clock-stop");
+const $musicInit = document.querySelector(".music-init");
+const $musicStop = document.querySelector(".music-stop");
+const $clock = document.querySelector(".clock");
+
+let clockTime;
+
+function startClock() {
+	clockTime = setInterval(() => {
+		let clockHour = new Date().toLocaleTimeString();
+		$clock.innerHTML = `<h3>${clockHour}</h3>`;
+	}, 1000);
+	$clockInit.disabled = true;
+}
+function stopClock() {
+	clearInterval(clockTime);
+	$clock.innerHTML = null;
+	$clockInit.disabled = false;
+}
+
+let musicTime;
+const $music = document.createElement("audio");
+
+function startMusic() {
+	$music.src = "../assets/05 Mr. Brownstone.mp3";
+	musicTime = setTimeout(() => {
+		$music.play();
+	}, 1000);
+	$musicInit.disabled = true;
+}
+function stopMusic() {
+	clearTimeout(musicTime);
+	$music.pause();
+	$music.currentTime = 0;
+	$musicInit.disabled = false;
+}
+
+$clockInit.addEventListener("click", startClock);
+$clockStop.addEventListener("click", stopClock);
+$musicInit.addEventListener("click", startMusic);
+$musicStop.addEventListener("click", stopMusic);
+
+// Shortcuts ********************************************
