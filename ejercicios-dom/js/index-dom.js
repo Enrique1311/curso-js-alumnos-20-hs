e4be71de1ca41c081c7da495dbbb073045d7a3f4
Enrique1311 <ejspinelli131176@gmail.com>
2023-04-18 23:58:43 -0300

    Mover objetos con las teclas y contador

--- ejercicios-dom/js/index-dom.js
+++ ejercicios-dom/js/index-dom.js
@@ -57,4 +57,52 @@ $clockStop.addEventListener("click", stopClock);
 $musicInit.addEventListener("click", startMusic);
 $musicStop.addEventListener("click", stopMusic);
 
-// Shortcuts ********************************************
+// Eventos del Teclado ********************************************
+const $ball = document.querySelector(".ball");
+const $stage = document.querySelector(".stage");
+let x = 0;
+let y = 0;
+
+function moveBall(e) {
+	let limitsBall = $ball.getBoundingClientRect();
+	let limitsStage = $stage.getBoundingClientRect();
+
+	switch (e.keyCode) {
+		case 37:
+			e.preventDefault();
+			if (limitsBall.left > limitsStage.left + 3) x--;
+			break;
+		case 38:
+			e.preventDefault();
+			if (limitsBall.top > limitsStage.top) y--;
+			break;
+		case 39:
+			e.preventDefault();
+			if (limitsBall.right < limitsStage.right - 3) x++;
+			break;
+		case 40:
+			e.preventDefault();
+			if (limitsBall.bottom < limitsStage.bottom) y++;
+			break;
+		default:
+			break;
+	}
+	$ball.style.transform = `translate(${x * 5}px,${y * 5}px)`;
+}
+
+document.addEventListener("keydown", moveBall);
+
+// Cuenta Regresiva ************************************************
+const $countdown = document.querySelector(".countdown");
+const countdownDate = new Date(limitDate).getTime();
+function countdown() {
+	let countdownTime = setInterval(() => {
+		let now = new Date().getTime();
+		let limitTime = countdownDate - now;
+		let days, hours, minutes, seconds;
+
+		$countdown.innerHTML = `Faltan ${days} días ${hours} horas ${minutes} minutos ${seconds} segundos para ${limitTime}`;
+	}, 1000);
+}
+
+countdown();
