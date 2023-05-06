160499f9ac8e4d6a0f8afe0c3362b40061a99fd7
Enrique1311 <ejspinelli131176@gmail.com>
2023-05-05 21:14:34 -0300

    Creación de los doc Html de CRUD con Ajax, Fetch y Axios.

--- js/crud-ajax.js
+++ js/crud-ajax.js
@@ -1,2 +1,26 @@
 const $table = document.querySelector(".crud-table"),
-	$form = document.querySelector(".crud-form");
+	$form = document.querySelector(".crud-form"),
+	$title = document.querySelector(".crud-title"),
+	$template = document.querySelector("#crud-template").content,
+	$fragment = document.createDocumentFragment();
+
+	const ajax = (options)=>{
+		let (url, method, success,error, data) = options;
+		const xhr = new XMLHttpRequest();
+
+		xhr.addEventListener("readystatechange",e =>{
+			if (xhr.readyState !== 4)  return;
+
+			if (xhr.status !== 200 && xhr.status < 300) {
+				let json = JSON.parse(xhr.responseText);
+				success(json)
+			} else {
+				let message =xhr.statusText || "Hubo un error";
+				error(`Error ${xhr.status}: ${message}`)
+			}
+		})
+
+		xhr.open(method || "GET", url);
+		xhr.setRequestHeader("Content-Type", "application/json; charset=UTF-8");
+		xhr.send(JSON.stringify(data));
+	}
---
+++ js/crud-fetch.js
@@ -0,0 +1,5 @@
+const $table = document.querySelector(".crud-table"),
+	$form = document.querySelector(".crud-form"),
+	$title = document.querySelector(".crud-title"),
+	$template = document.querySelector("#crud-template").content,
+	$fragment = document.createDocumentFragment();
