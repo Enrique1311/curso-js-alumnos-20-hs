bfd26f821a59e0cc00ae89cfaed8f6f17a1a5392
Enrique1311 <ejspinelli131176@gmail.com>
2023-05-29 22:52:41 -0300

    Creación de las cards

--- ejercicios-ajax/stripe/stripe-checkout.js
+++ ejercicios-ajax/stripe/stripe-checkout.js
@@ -1,7 +1,7 @@
 import STRIPE_KEYS from "./stripe-keys.js";
 
 const $products = document.getElementById("products"),
-	$template = document.getElementById("product-template").contentEditable,
+	$template = document.getElementById("product-template").content,
 	$fragment = document.createDocumentFragment(),
 	fetchOptions = {
 		headers: {
@@ -9,11 +9,39 @@ const $products = document.getElementById("products"),
 		},
 	};
 
-let prices, products;
+let products, prices;
 
 Promise.all([
 	fetch("https://api.stripe.com/v1/products", fetchOptions),
 	fetch("https://api.stripe.com/v1/prices", fetchOptions),
 ])
 	.then((responses) => Promise.all(responses.map((res) => res.json())))
-	.then((json) => console.log(json));
+	.then((json) => {
+		//console.log(json);
+		products = json[0].data;
+		prices = json[1].data;
+		console.log(products, prices);
+
+		prices.forEach((el) => {
+			let productData = products.filter((product) => product.id === el.product);
+			console.log(productData);
+
+			$template.querySelector(".product").setAttribute("data-price", el.id);
+			$template.querySelector("img").src = productData[0].images[0];
+			$template.querySelector("img").alt = productData[0].name;
+			$template.querySelector(
+				"figcaption"
+			).innerHTML = `${productData[0].name} <br> ${el.currency} ${el.unit_amount_decimal} `;
+
+			let $clone = document.importNode($template, true);
+			$fragment.appendChild($clone);
+		});
+
+		$products.appendChild($fragment);
+	})
+
+	.catch((err) => {
+		console.log(err);
+		let message = err.statusText || "Ocurrió un error al conectarse con Stripe";
+		$products.innerHTML = `<p>Error ${err.status}; ${message}</p>`;
+	});
